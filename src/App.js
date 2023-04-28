275f99ea15867d16f2167935918f515f2d4da0d4
Alaics <acerlaptop0619@gmail.com>
2023-04-28 15:36:52 +0800

    first commitsadasd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+// adasdasda
 import './App.css';
+import { Container } from "react-bootstrap";
+import React from "react"
+import './bootstrap.min.css'
+import { Header } from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
+import { HomeScreen } from './screens/HomeScreen'
+import  Login  from './SignUp/Login'
+import { Logout } from './SignUp/Logout'
+import Register   from './SignUp/Register'
+import SubscriptionScreen from './screens/SubscriptionScreen'
+import  MoviePlaybackScreen   from './screens/MoviePlaybackScreen'
+import  User  from "./SignUp/User"
+import ProfileScreen from './screens/ProfileScreen'
+import AboutScreen from './screens/AboutScreen'
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/User' element={<User />}  />
+              <Route path='/AboutScreen' element={<AboutScreen />}  />
+              <Route  path="/MoviePlaybackScreen/:id" element={<MoviePlaybackScreen />}  />
+              <Route path='/ProfileScreen' element={<ProfileScreen />}  />
+              <Route path='/SubscriptionScreen' element={<SubscriptionScreen />}  />
+              <Route path='/ProductScreen' element={<ProductScreen />}  />
+              <Route path='/Logout' element={<Logout />}  />
+              <Route path='/login' element={<Login />}  />
+              <Route path='/Product/:id' element={<ProductScreen />} />
+              <Route path='/register' element={<Register />}  />
+              
+              
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/SignUp/Register.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import ppp from './video/ppp.mp4'
+import { register } from '../actions/userActions'
+
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js"
+function Register() {
+    // const amount = 2
+    const initialOptions = {
+        "client-id": "AYNnCSQdXD2Kuu2aKDoXOeiHyGdmch03IfqadvsPvh8f3Ucz9azwC1_sLP0JfVS9kK1jHimmoVSjg9hf",
+        currency: "USD",
+        intent: "subscription",
+        vault: true,
+      };
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+    const location = useLocation()
+    const dispatch = useDispatch()
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { error, loading, userInfo } = userLogin
+
+    let navigate = useNavigate()
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect)
+        }
+    }, [userInfo, navigate, redirect])
+
+    const submitHandler = (e) => {
+      e.preventDefault()
+
+      if (password != confirmPassword) {
+          setMessage('Passwords do not match')
+      } else {
+          dispatch(register(name, email, password))
+      }
+
+  }
+
+  const successPaymentHandler = (e) => {
+
+    if (password !== confirmPassword) {
+        setMessage('Passwords do not match')
+    } else {
+        dispatch(register(name, email, password))
+    }
+
+}
+    return (
+       <div>
+            <video className='source' src={ppp} autoPlay loop muted style={{ position: 'absolute', top: 450, left: 960, width: '100%', height: '100%', objectFit: 'cover' }}>
+        <source src={ppp} type="video/mp4" />
+      </video>
+      <div className='card'>
+            {message && <Message variant='danger'>{message}</Message>}
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        required
+                        type='email'
+                        placeholder='Enter Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Enter Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='passwordConfirm'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+                <div className='text-center'>
+
+                <PayPalScriptProvider options={{"client-id": "AYNnCSQdXD2Kuu2aKDoXOeiHyGdmch03IfqadvsPvh8f3Ucz9azwC1_sLP0JfVS9kK1jHimmoVSjg9hf"}}>
+               <PayPalButtons classname='text-center'type='submit' variant='primary'
+                // amount={amount}
+                onApprove={successPaymentHandler}
+                />
+
+          </PayPalScriptProvider>
+          </div>
+
+            </Form>
+
+            <Row className='py-3'>
+                <Col>
+                    Have an Account? <Link
+                        to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                        Sign In
+                        </Link>
+                </Col>
+            </Row>
+            </div>
+            </div>
+    )
+}
+
+export default Register
